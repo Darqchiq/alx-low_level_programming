8dac2f33715e49cbc6c0f9b79e33545fa67d6ea7
Darqchiq <kinya277@gmail.com>
2023-04-23 04:34:00 -0700

    4puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <stadio.h
+#include <stdio.h>
 /**
- * main a program that prints a line with puts function
- * return: 0
+ * main - A program that prints a line with puts function
+ * return: 0 (succesful)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
