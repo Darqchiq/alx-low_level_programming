719c42d07b70776672c661304d6f6fc73f4d810e
Darqchiq <kinya277@gmail.com>
2023-04-29 13:36:07 -0700

    3print

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,19 +6,13 @@
  */
 int main(void)
 {
-	int n = 97;
-	int m = 65;
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int i;
 
-	while (n <= 122)
-	{
-		putchar(n);
-		n++;
-	}
-	while (m <= 90)
-	{
-		putchar(m);
-		n++;
-	}
-	putchar('\n');
-	return (0);
+for (i = 0; i < 52; i++)
+{
+        putchar(alp[i]);
+        }
+        putchar('\n');
+        return (0);
 }
