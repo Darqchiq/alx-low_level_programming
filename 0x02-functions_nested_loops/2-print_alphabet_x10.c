30b9e4631a0372e5cf8357cb1b7d6f1452f4386a
Darqchiq <kinya277@gmail.com>
2023-05-07 12:34:51 -0700

    2print

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,10 +2,10 @@
 
 
 	/**
-	 * print_alphabetx10 - prints 10 times the alphabet, in lowercase,
+	 * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
 	 * followed by a new line
 	 */
-	void print_alphabetx10(void)
+	void print_alphabet_x10(void)
 	{
 		char ch;
 		int i;
