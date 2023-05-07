7e908458903ec04a030008c15af1a51e34826abc
Darqchiq <kinya277@gmail.com>
2023-05-07 06:43:47 -0700

    6putsc

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,18 +1,15 @@
 #include "main.h"
 /**
- * swap_int - Takes in two varriable integers and swaaps them
- * @author Anisah
- * @a: swaps and stores address of b
- * @b: swaps and stores adress of a
- * Return: 0
+ * swap_int - swaps the values of two integers
+ * @a: integer to swap
+ * @b: integer to swap
  */
-
 void swap_int(int *a, int *b)
 {
-	int change;
+	int m;
 
-	change = *b;
-
-	*b = *a;
-	*a = change;
+	m = *a;
+	*a = *b;
+	*b = m;
 }
+
